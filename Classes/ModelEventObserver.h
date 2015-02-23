edf2a927a3d0e47d9958a1d41252f93b995f4eb9
pankona <yosuke.akatsuka@gmail.com>
2015-02-23 18:45:32 +0900

    remove unnecessary files

--- Classes/ModelEventObserver.h
+++
@@ -1,9 +0,0 @@
-#ifndef __MODELEVENTOBSERVER_H__
-#define __MODELEVENTOBSERVER_H__
-
-class ModelEventObserver {
-public:
-    virtual void onUpdate() = 0;
-};
-
-#endif /* __MODELEVENTOBSERVER_H__ */

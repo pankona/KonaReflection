4dc2115ac922a4489e24e016151880eba448f10a
pankona <yosuke.akatsuka@gmail.com>
2015-04-02 20:10:21 +0900

    [#38] Remove duplicated code to move them to parent class.

--- Classes/GameScene/DrawableCongrat.cpp
+++ Classes/GameScene/DrawableCongrat.cpp
@@ -34,52 +34,9 @@ DrawableCongrat::DrawableCongrat(int in_width, int in_height) {
     baseLayer->addChild(dBackToTitleLabel->getNode());
 }
 
-Layer*
-DrawableCongrat::getLayer() {
+Node*
+DrawableCongrat::getNode() {
     return baseLayer;
-}
-
-void
-DrawableCongrat::addViewEventListener(ViewEventListener* in_listener) {
-
-    listeners.push_back(in_listener);
-
-    auto onTouchListener = EventListenerTouchOneByOne::create();
-    onTouchListener->onTouchBegan = [this](Touch* touch, Event* event) {
-        Position p;
-        p.x = (int) touch->getLocation().x;
-        p.y = (int) touch->getLocation().y;
-        for (ViewEventListener* listener : listeners) {
-            listener->onTouchBegan(event->getCurrentTarget(), p, NULL);
-        }
-
-        return true;
-    };
-
-    onTouchListener->onTouchMoved = [this](Touch* touch, Event* event) {
-        Position p;
-        p.x = (int) touch->getLocation().x;
-        p.y = (int) touch->getLocation().y;
-        for (ViewEventListener* listener : listeners) {
-            listener->onTouchMoved(event->getCurrentTarget(), p, NULL);
-        }
-    };
-
-    onTouchListener->onTouchEnded = [this](Touch* touch, Event* event) {
-        for (ViewEventListener* listener : listeners) {
-            listener->onTouchEnded(event->getCurrentTarget(), NULL);
-        }
-    };
-
-    Director::getInstance()->getEventDispatcher()->addEventListenerWithSceneGraphPriority(onTouchListener, baseLayer);
-}
-
-void
-DrawableCongrat::removeViewEventListener(ViewEventListener* in_listener) {
-    auto it = std::find(listeners.begin(), listeners.end(), in_listener);
-    if (it != listeners.end()) {
-        listeners.erase(it);
-    }
 }
 
 void
